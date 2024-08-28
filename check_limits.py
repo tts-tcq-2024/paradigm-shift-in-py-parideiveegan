86a46101f4a2f69af389d697a4c476cad442a4ca
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-08-28 17:20:53 +0530

    Update check_limits.py
    added asserts to check the all logic possibilities

--- check_limits.py
+++ check_limits.py
@@ -7,23 +7,9 @@ def check_chargeRate(charge_rate):
 def battery_is_ok(temperature, soc, charge_rate):
   return check_temperature(temperature) and check_soc(soc) and check_chargeRate(charge_rate)
  
-  
-  
-
-'''  
-if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range !')
-    return False 
-'''
-  
-
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(40, 80, 0.6) is False)
+  assert(battery_is_ok(30, 60, 0.8) is False)
