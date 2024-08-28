d584ea12c6d3b93cfe60662ec8e3eaa06e135191
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-08-28 14:49:34 +0530

    Update check_limits.py
    1) Excluded all the print statements. 
    2) The conditions are put in one if statement

--- check_limits.py
+++ check_limits.py
@@ -1,6 +1,11 @@
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
+  if (temperature < 0 or temperature > 45) or (soc < 20 or soc > 80) or (charge_rate > 0.8):
+    return False
+
+
+'''  
+if temperature < 0 or temperature > 45:
     print('Temperature is out of range!')
     return False
   elif soc < 20 or soc > 80:
@@ -8,7 +13,8 @@ def battery_is_ok(temperature, soc, charge_rate):
     return False
   elif charge_rate > 0.8:
     print('Charge rate is out of range !')
-    return False
+    return False 
+'''
 
   return True
 
