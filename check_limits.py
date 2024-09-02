cb4740500e5a8a2c1722641ade30cf07ad0be1c8
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-09-02 21:41:12 +0530

    Update check_limits.py
    removing duplicate functions

--- check_limits.py
+++ check_limits.py
@@ -1,11 +1,20 @@
-def check_temperature(temperature):
-  return (0< temperature <45)
-def check_soc(soc):
-  return (20< soc < 80)
-def check_chargeRate(charge_rate):
-  return (charge_rate < 0.8)
-def battery_is_ok(temperature, soc, charge_rate):
-  return check_temperature(temperature) and check_soc(soc) and check_chargeRate(charge_rate)
+def check_parameter_breach(parameter_value, parameter_name, min, max):
+  breach_status = "Normal
+  range_status = "is in range"
+  if (parameter_value < min):
+    breach_status = "Low"
+    range_status = "is out of range"
+  elif(parameter_value > max):
+    breach_status = "High"
+    range_status = "is out of range"
+  printf(parameter_name+" "+range_status+", the value is "+breach_status)
+  
+def check_parameter_range(parameter_value, parameter_name, min, max):
+  check_parameter_breach(parameter_value, parameter_name, min, max)
+  return(min<parameter_value<max)
+  
+def battery_is_ok(temperature, soc, charge_rate):  
+  return check_parameter_range(temperature,"Temperature",0,45) and check_parameter_range(soc,"SOC",20,80) and check_parameter_range(charge_rate,"charge_rate",0,0.8)
  
 
 if __name__ == '__main__':
