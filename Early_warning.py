cae4a6fcaf48c2ef544494c2c56a03d8bd1397f9
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-10-13 23:16:42 +0530

    Update Early_warning.py

--- Early_warning.py
+++ Early_warning.py
@@ -1,9 +1,9 @@
-def set_warning_status(parameter_value, parameter_name, min, max,breach_status, range_status):
-  if (parameter_value <= min):
+def set_warning_status(parameter_value, parameter_name, min, max,breach_status, range_status, tolerance):
+  if (min < parameter_value < min+tolerance):
     breach_status = "Warning"
     range_status = "Approaching discharge"
     
-  elif(parameter_value >= max):
+  elif(max-tolerance < parameter_value < max):
     breach_status = "Warning"
     range_status = "Approaching charge-peak"
   return [breach_status, range_status]
@@ -11,9 +11,7 @@ def set_warning_status(parameter_value, parameter_name, min, max,breach_status, 
 def get_warning(parameter_value, parameter_name, min, max, key, breach_status, range_status):
   if not key:
     return [breach_status, range_status]
-  parameter_value_tolerance = (parameter_value*5)/100
-  min = min + parameter_value_tolerance
-  max = max - parameter_value_tolerance
-  if not (min<parameter_value<max):
-    return set_warning_status(parameter_value, parameter_name, min, max,breach_status, range_status)
+  tolerance = (parameter_value*5)/100
+  if not (min+tolerance < parameter_value < max-tolerance):
+    return set_warning_status(parameter_value, parameter_name, min, max,breach_status, range_status, tolerance)
   return [breach_status, range_status]
